4eb8ab1590490e4d25fe4a22729b84e4d52fc297
JoseCordage <jose@cordage.io>
2021-11-02 14:57:17 -0600

    Swagger docs of Session

--- lib/hangman_web/controllers/session_controller.ex
+++ lib/hangman_web/controllers/session_controller.ex
@@ -6,13 +6,69 @@ defmodule HangmanWeb.SessionController do
 
     action_fallback HangmanWeb.UserErrorController
 
+    def swagger_definitions do
+        %{
+          User:
+            swagger_schema do
+              title("User")
+              description("Managers for the words list")
+
+              properties do
+                id(:integer, "User ID")
+                name(:string, "User name", required: true)
+                credential(Schema.ref(:Credential))
+                inserted_at(:string, "Creation timestamp", format: :datetime)
+                updated_at(:string, "Update timestamp", format: :datetime)
+              end
+            end,
+          Credential:
+            swagger_schema do
+              title("Credential")
+              description("Managers credential")
+
+              properties do
+                id(:integer, "Credential ID")
+                email(:string, "User Email", required: true)
+                password_hash(:string)
+                password(:string, "Virtual Password")
+                password_confirmation(:string, "Virtual Password Confirmation")
+                admin(:boolean, "User is a Super Manenger", required: true)
+                active(:boolean, "User has verified email", required: true)
+                user(Schema.ref(:User))
+                inserted_at(:string, "Creation timestamp", format: :datetime)
+                updated_at(:string, "Update timestamp", format: :datetime)
+              end
+            end,
+        CreateSessionRequest:
+        swagger_schema do
+          title("CreateUserRequest")
+          description("POST body for creating a user")
+          property(:users, Schema.array(:User), "The users details")
+          example(%{
+              email: "juan@cordage.io",
+              password: "Qwerty2021"
+          })
+        end,
+        CreateSessionResponse:
+            swagger_schema do
+            title("CreateSessionResponse")
+            description("Response and id")
+            property(:users, Schema.array(:User), "The users details")
+            example(%{
+                user_id: 1
+            })
+            end
+        }
+      end
+
     swagger_path :create_session do
         post("/api/login")
         summary("Create session")
         description("Return the id of the user if it's found and the password is valid and create a session")
-        response(code(:ok), "Success")
         produces("application/json")
         deprecated(false)
+        parameter(:user, :body, Schema.ref(:CreateSessionRequest), "The user details")
+        response(200, "Success",Schema.ref(:CreateSessionResponse))
     end
 
     def create_session(conn, %{"email" => email, "password" => password}) do
@@ -34,7 +90,7 @@ defmodule HangmanWeb.SessionController do
         delete("/api/logout")
         summary("Delete session")
         description("Delete the session")
-        response(code(205), "No Response")
+        response(code(205), "Session ended")
         produces("application/json")
         deprecated(false)
     end
