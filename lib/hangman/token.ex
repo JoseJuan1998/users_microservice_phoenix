4cb4555649711a3b4a3f5bda15995f1f2e030332
JoseCordage <jose@cordage.io>
2021-12-17 15:46:50 -0600

    Tokens fixed

--- lib/hangman/token.ex
+++ lib/hangman/token.ex
@@ -5,12 +5,12 @@ defmodule Hangman.Token do
   """
   @spec auth_sign(map()) :: binary()
   def auth_sign(data) do
-    Phoenix.Token.sign(HangmanWeb.Endpoint, "auth", data)
+    Phoenix.Token.sign("VAMWT81mVrqjNQ8qSNlnQIls5PFnZFHe", "auth", data)
   end
 
   @spec email_sign(map()) :: binary()
   def email_sign(data) do
-    Phoenix.Token.sign(HangmanWeb.Endpoint, "email", data)
+    Phoenix.Token.sign("VAMWT81mVrqjNQ8qSNlnQIls5PFnZFHe", "email", data)
   end
 
   @doc """
@@ -20,13 +20,13 @@ defmodule Hangman.Token do
   """
   @spec verify_auth(String.t()) :: {:ok, any()} | {:error, :unauthenticated}
   def verify_auth(token) do
-    Phoenix.Token.verify(HangmanWeb.Endpoint, "auth", token, max_age: 3600)
+    Phoenix.Token.verify("VAMWT81mVrqjNQ8qSNlnQIls5PFnZFHe", "auth", token, max_age: 3600)
   end
 
   # coveralls-ignore-start
   @spec verify_email(String.t()) :: {:ok, any()} | {:error, :unauthenticated}
   def verify_email(token) do
-    Phoenix.Token.verify(HangmanWeb.Endpoint, "email", token, max_age: 86400*7)
+    Phoenix.Token.verify("VAMWT81mVrqjNQ8qSNlnQIls5PFnZFHe", "email", token, max_age: 86400*7)
   end
   # coveralls-ignore-stop
 end
